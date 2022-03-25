98c6b08fe2b0a7e16718b0fea5fcdca85f6085ec
Satyam Tripathi <satyamtripathi5554@gmail.com>
2022-03-25 23:44:52 +0530

    Add files via upload

---
+++ Day2/Majority_Element.py
@@ -0,0 +1,9 @@
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        left = 0
+        right = sum(nums)
+        for idx, val in enumerate(nums):
+            right -= val
+            if left == right:
+                return idx
+            left += val
